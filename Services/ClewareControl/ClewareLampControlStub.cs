f3183a7dc0aafcf6773d48acdbddca296e5e9243
Benj Huser <benj.huser@gmail.com>
2020-07-19 17:07:49 +0200

    Create stub control for development

---
+++ Services/ClewareControl/ClewareLampControlStub.cs
@@ -0,0 +1,35 @@
+﻿using busylight.Model;
+using System;
+using System.Collections.Generic;
+
+namespace busylight.Services.ClewareControl
+{
+    public class ClewareLampControlStub : IClewareLampControl
+    {
+        private readonly TrafficLight trafficLight;
+
+        public ClewareLampControlStub()
+        {
+            trafficLight =
+                new TrafficLight(0, new[]
+                {
+                    new Lamp(0, LampColor.Red),
+                    new Lamp(1, LampColor.Orange),
+                    new Lamp(2, LampColor.Green),
+                });
+        }
+
+        public IEnumerable<TrafficLight> DiscoverConnectedDevices()
+        {
+            return new[]{ trafficLight };
+        }
+
+        public void Dispose()
+        {
+        }
+
+        public void SwitchLight(int deviceId, int lampId, bool isOn)
+        {
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -13,87 +13,97 @@ using System.Runtime.InteropServices;
 
 namespace busylight
 {
-   public class Startup
-   {
-      public Startup(IConfiguration configuration)
-      {
-         Configuration = configuration;
-      }
+    public class Startup
+    {
+        private readonly IWebHostEnvironment environment;
 
-      public IConfiguration Configuration { get; }
+        public Startup(IConfiguration configuration, IWebHostEnvironment environment)
+        {
+            Configuration = configuration;
+            this.environment = environment;
+        }
 
-      // This method gets called by the runtime. Use this method to add services to the container.
-      public void ConfigureServices(IServiceCollection services)
-      {
-         services.AddSingleton<ITrafficLightRepository, TrafficLightRepository>();
+        public IConfiguration Configuration { get; }
 
-         if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
-         {
-            services.AddSingleton<IClewareLampControl, ClewareLampControlWindows>();
-         }
-         else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
-         {
-            services.AddSingleton<IClewareLampControl, ClewareLampControlLinux>();
-         }
-         else
-         {
-            throw new InvalidOperationException("Cannot run on MacOs");
-         }
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton<ITrafficLightRepository, TrafficLightRepository>();
 
-         services.AddControllersWithViews()
-            .AddNewtonsoftJson(options =>
-         {
-            options.SerializerSettings.Converters.Add(new Newtonsoft.Json.Converters.StringEnumConverter());
-            options.SerializerSettings.NullValueHandling = Newtonsoft.Json.NullValueHandling.Ignore;
-         });
+            if (environment.IsDevelopment())
+            {
+                services.AddSingleton<IClewareLampControl, ClewareLampControlStub>();
+            }
+            else
+            {
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                {
+                    services.AddSingleton<IClewareLampControl, ClewareLampControlWindows>();
+                }
+                else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+                {
+                    services.AddSingleton<IClewareLampControl, ClewareLampControlLinux>();
+                }
+                else
+                {
+                    throw new InvalidOperationException("Cannot run on MacOs");
+                }
+            }
 
-         // In production, the React files will be served from this directory
-         services.AddSpaStaticFiles(configuration =>
-         {
-            configuration.RootPath = "ClientApp/build";
-         });
+            services.AddControllersWithViews()
+               .AddNewtonsoftJson(options =>
+            {
+                options.SerializerSettings.Converters.Add(new Newtonsoft.Json.Converters.StringEnumConverter());
+                options.SerializerSettings.NullValueHandling = Newtonsoft.Json.NullValueHandling.Ignore;
+            });
 
-         services.AddSignalR();
-      }
+            // In production, the React files will be served from this directory
+            services.AddSpaStaticFiles(configuration =>
+            {
+                configuration.RootPath = "ClientApp/build";
+            });
 
-      // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-      public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-      {
-         if (env.IsDevelopment())
-         {
-            app.UseDeveloperExceptionPage();
-         }
-         else
-         {
-            app.UseExceptionHandler("/Error");
-            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-            app.UseHsts();
-         }
+            services.AddSignalR();
+        }
 
-         app.UseHttpsRedirection();
-         app.UseStaticFiles();
-         app.UseSpaStaticFiles();
-
-         app.UseRouting();
-
-         app.UseEndpoints(endpoints =>
-         {
-            endpoints.MapControllerRoute(
-                   name: "default",
-                   pattern: "{controller}/{action=Index}/{id?}");
-
-            endpoints.MapHub<TrafficLightHub>("/hub");
-         });
-
-         app.UseSpa(spa =>
-         {
-            spa.Options.SourcePath = "ClientApp";
-
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
             if (env.IsDevelopment())
             {
-               spa.UseReactDevelopmentServer(npmScript: "start");
+                app.UseDeveloperExceptionPage();
             }
-         });
-      }
-   }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseSpaStaticFiles();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller}/{action=Index}/{id?}");
+
+                endpoints.MapHub<TrafficLightHub>("/hub");
+            });
+
+            app.UseSpa(spa =>
+            {
+                spa.Options.SourcePath = "ClientApp";
+
+                if (env.IsDevelopment())
+                {
+                    spa.UseReactDevelopmentServer(npmScript: "start");
+                }
+            });
+        }
+    }
 }
