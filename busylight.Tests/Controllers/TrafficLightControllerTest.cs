bfae648863156f21ae5d39a7a4c52f4caea0ef0e
Benj Huser <benj.huser@gmail.com>
2020-07-20 14:52:31 +0200

    Add tests for TrafficLightController

--- busylight.Tests/Controllers/TrafficLightControllerTest.cs
+++ busylight.Tests/Controllers/TrafficLightControllerTest.cs
@@ -6,8 +6,9 @@ using busylight.Services.ClewareControl;
 using Microsoft.AspNetCore.SignalR;
 using Microsoft.Extensions.Logging;
 using Moq;
-using System;
 using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 using Xunit;
 
 namespace busylight.Tests
@@ -15,17 +16,28 @@ namespace busylight.Tests
     public class TrafficLightControllerTest
     {
         private readonly ITrafficLightRepository trafficLightRepo;
+        private readonly ILogger<TrafficLightController> logger;
+        private readonly IClewareLampControl clewareLampControl;
         private readonly TrafficLightController trafficLightController;
+        private IClientProxy clientProxy;
 
         public TrafficLightControllerTest()
         {
             trafficLightRepo = Mock.Of<ITrafficLightRepository>();
 
+            logger = Mock.Of<ILogger<TrafficLightController>>();
+            clewareLampControl = Mock.Of<IClewareLampControl>();
+
+            var hubContextMock = new Mock<IHubContext<TrafficLightHub>>();
+
+            clientProxy = Mock.Of<IClientProxy>();
+            hubContextMock.SetupGet(x => x.Clients.All).Returns(clientProxy);
+
             trafficLightController = new TrafficLightController(
-                Mock.Of<ILogger<TrafficLightController>>(),
+                logger,
                 trafficLightRepo,
-                Mock.Of<IClewareLampControl>(),
-                Mock.Of<IHubContext<TrafficLightHub>>());
+                clewareLampControl,
+                hubContextMock.Object);
         }
 
         [Fact]
@@ -50,6 +62,161 @@ namespace busylight.Tests
             var actual = trafficLightController.Get(trafficLightId);
 
             Assert.Equal(trafficLight, actual);
+        }
+
+        [Fact]
+        public void GetLamps_GivenTrafficLightID_ReturnsLamps()
+        {
+            var greenLamp = new Lamp(1, LampColor.Green);
+            var redLamp = new Lamp(0, LampColor.Red);
+            var lamps = new[] { greenLamp, redLamp };
+
+            var trafficLight = new TrafficLight(0, lamps);
+            Mock.Get(trafficLightRepo).Setup(x => x.GetTrafficLightById(0)).Returns(trafficLight);
+
+            var actualLamps = trafficLightController.GetLamps(0);
+
+            Assert.Equal(lamps, actualLamps);
+        }
+
+        [Fact]
+        public void GetLamp_GivenTrafficLightAndLampID_ReturnsLamp()
+        {
+            var greenLamp = new Lamp(1, LampColor.Green);
+            var trafficLight = new TrafficLight(0, new[] { greenLamp });
+            Mock.Get(trafficLightRepo).Setup(x => x.GetTrafficLightById(0)).Returns(trafficLight);
+
+            var actualLamp = trafficLightController.GetLamp(0, 1);
+
+            Assert.Equal(greenLamp, actualLamp);
+        }
+
+        [Fact]
+        public void GetLamp_GivenNoMatchingLampID_ReturnsNull()
+        {
+            var greenLamp = new Lamp(1, LampColor.Green);
+            var trafficLight = new TrafficLight(0, new[] { greenLamp });
+            Mock.Get(trafficLightRepo).Setup(x => x.GetTrafficLightById(0)).Returns(trafficLight);
+
+            var actualLamp = trafficLightController.GetLamp(0, 12);
+
+            Assert.Null(actualLamp);
+        }
+
+        [Theory]
+        [InlineData(true)]
+        [InlineData(false)]
+        public async Task GivenLamp_PutIntoDifferentState_SetsLampStateToNewState(bool newLampState)
+        {
+            var greenLamp = new Lamp(1, LampColor.Green);
+            greenLamp.SetLampState(!newLampState);
+            var trafficLight = new TrafficLight(0, new[] { greenLamp });
+            Mock.Get(trafficLightRepo).Setup(x => x.GetTrafficLightById(0)).Returns(trafficLight);
+
+            await trafficLightController.Put(0, 1, newLampState);
+
+            Assert.Equal(newLampState, greenLamp.IsOn);
+        }
+
+        [Theory]
+        [InlineData(true)]
+        [InlineData(false)]
+        public async Task GivenLamp_PutIntoDifferentState_SwitchesLampState(bool newLampState)
+        {
+            var greenLamp = new Lamp(1, LampColor.Green);
+            greenLamp.SetLampState(!newLampState);
+            var trafficLight = new TrafficLight(0, new[] { greenLamp });
+            Mock.Get(trafficLightRepo).Setup(x => x.GetTrafficLightById(0)).Returns(trafficLight);
+
+            await trafficLightController.Put(0, 1, newLampState);
+
+            Mock.Get(clewareLampControl).Verify(x => x.SwitchLight(0, 1, newLampState));
+        }
+
+        [Theory]
+        [InlineData(true)]
+        [InlineData(false)]
+        public async Task GivenLamp_PutIntoDifferentState_NotifiesClients(bool newLampState)
+        {
+            var greenLamp = new Lamp(1, LampColor.Green);
+            greenLamp.SetLampState(!newLampState);
+            var trafficLight = new TrafficLight(0, new[] { greenLamp });
+            Mock.Get(trafficLightRepo).Setup(x => x.GetTrafficLightById(0)).Returns(trafficLight);
+
+            await trafficLightController.Put(0, 1, newLampState);
+
+            Mock.Get(clientProxy).Verify(x => x.SendCoreAsync("trafficLightUpdate", new object[] { 0 }, It.IsAny<CancellationToken>()));
+        }
+
+        [Theory]
+        [InlineData(true)]
+        [InlineData(false)]
+        public async Task GivenLamp_PutIntoSameState_DoesNotChangeState(bool lampState)
+        {
+            var greenLamp = new Lamp(1, LampColor.Green);
+            greenLamp.SetLampState(lampState);
+            var trafficLight = new TrafficLight(0, new[] { greenLamp });
+            Mock.Get(trafficLightRepo).Setup(x => x.GetTrafficLightById(0)).Returns(trafficLight);
+
+            await trafficLightController.Put(0, 1, lampState);
+
+            Assert.Equal(lampState, greenLamp.IsOn);
+        }
+
+        [Theory]
+        [InlineData(true)]
+        [InlineData(false)]
+        public async Task GivenLamp_PutIntoSameState_DoesNotSwitchLights(bool lampState)
+        {
+            var greenLamp = new Lamp(1, LampColor.Green);
+            greenLamp.SetLampState(lampState);
+            var trafficLight = new TrafficLight(0, new[] { greenLamp });
+            Mock.Get(trafficLightRepo).Setup(x => x.GetTrafficLightById(0)).Returns(trafficLight);
+
+            await trafficLightController.Put(0, 1, lampState);
+
+            Mock.Get(clewareLampControl).Verify(x => x.SwitchLight(0, 1, lampState), Times.Never);
+        }
+
+        [Theory]
+        [InlineData(true)]
+        [InlineData(false)]
+        public async Task GivenLamp_PutIntoSameState_DoesNotNotifyClients(bool lampState)
+        {
+            var greenLamp = new Lamp(1, LampColor.Green);
+            greenLamp.SetLampState(lampState);
+            var trafficLight = new TrafficLight(0, new[] { greenLamp });
+            Mock.Get(trafficLightRepo).Setup(x => x.GetTrafficLightById(0)).Returns(trafficLight);
+
+            await trafficLightController.Put(0, 1, lampState);
+
+            Mock.Get(clientProxy).Verify(x => x.SendCoreAsync("trafficLightUpdate", new object[] { 0 }, It.IsAny<CancellationToken>()), Times.Never);
+        }
+
+        [Fact]
+        public async Task GivenLampID_DoesNotFindLamp_Logs()
+        {
+            var trafficLight = new TrafficLight(0, Enumerable.Empty<Lamp>());
+            Mock.Get(trafficLightRepo).Setup(x => x.GetTrafficLightById(0)).Returns(trafficLight);
+
+            await trafficLightController.Put(0, 0, true);
+
+            Mock.Get(logger).VerifyLog(Times.Once);
+        }
+
+        [Theory]
+        [InlineData(true)]
+        [InlineData(false)]
+        public async Task GivenLamp_PutIntoSameState_LogsMessage(bool lampState)
+        {
+            var greenLamp = new Lamp(1, LampColor.Green);
+            greenLamp.SetLampState(lampState);
+            var trafficLight = new TrafficLight(0, new[] { greenLamp });
+            Mock.Get(trafficLightRepo).Setup(x => x.GetTrafficLightById(0)).Returns(trafficLight);
+
+            await trafficLightController.Put(0, 1, lampState);
+
+            Mock.Get(logger).VerifyLog(Times.Once);
         }
     }
 }
--- busylight/Controllers/TrafficLightController.cs
+++ busylight/Controllers/TrafficLightController.cs
@@ -13,71 +13,87 @@ using Microsoft.Extensions.Logging;
 
 namespace busylight.Controllers
 {
-   [Route("api/[controller]")]
-   [ApiController]
-   public class TrafficLightController : ControllerBase
-   {
-      private readonly ILogger<TrafficLightController> logger;
-      private readonly ITrafficLightRepository trafficLightRepository;
-      private readonly IClewareLampControl clewareLampControl;
-      private readonly IHubContext<TrafficLightHub> trafficLightHub;
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TrafficLightController : ControllerBase
+    {
+        private readonly ILogger<TrafficLightController> logger;
+        private readonly ITrafficLightRepository trafficLightRepository;
+        private readonly IClewareLampControl clewareLampControl;
+        private readonly IHubContext<TrafficLightHub> trafficLightHub;
 
-      public TrafficLightController(
-         ILogger<TrafficLightController> logger,
-         ITrafficLightRepository trafficLightRepository,
-         IClewareLampControl clewareLampControl,
-         IHubContext<TrafficLightHub> trafficLightHub)
-      {
-         this.logger = logger;
-         this.trafficLightRepository = trafficLightRepository;
-         this.clewareLampControl = clewareLampControl;
-         this.trafficLightHub = trafficLightHub;
-      }
+        public TrafficLightController(
+           ILogger<TrafficLightController> logger,
+           ITrafficLightRepository trafficLightRepository,
+           IClewareLampControl clewareLampControl,
+           IHubContext<TrafficLightHub> trafficLightHub)
+        {
+            this.logger = logger;
+            this.trafficLightRepository = trafficLightRepository;
+            this.clewareLampControl = clewareLampControl;
+            this.trafficLightHub = trafficLightHub;
+        }
 
-      // GET: api/<TrafficLightController>
-      [HttpGet]
-      public IEnumerable<TrafficLight> Get()
-      {
-         logger.LogDebug("Getting traffic light");
-         return trafficLightRepository.GetTrafficLights();
-      }
+        // GET: api/<TrafficLightController>
+        [HttpGet]
+        public IEnumerable<TrafficLight> Get()
+        {
+            logger.LogDebug("Getting traffic light");
+            return trafficLightRepository.GetTrafficLights();
+        }
 
-      // GET api/<TrafficLightController>/5
-      [HttpGet("{id}")]
-      public TrafficLight Get(int id)
-      {
-         return trafficLightRepository.GetTrafficLightById(id);
-      }
+        // GET api/<TrafficLightController>/5
+        [HttpGet("{id}")]
+        public TrafficLight Get(int id)
+        {
+            return trafficLightRepository.GetTrafficLightById(id);
+        }
 
-      // GET api/<TrafficLightController>/5/lamps
-      [HttpGet("{id}/lamps")]
-      public IEnumerable<Lamp> GetLamps(int id)
-      {
-         return trafficLightRepository.GetTrafficLightById(id).Lamps;
-      }
+        // GET api/<TrafficLightController>/5/lamps
+        [HttpGet("{id}/lamps")]
+        public IEnumerable<Lamp> GetLamps(int id)
+        {
+            return trafficLightRepository.GetTrafficLightById(id).Lamps;
+        }
 
-      // GET api/<TrafficLightController>/5/lamps/2
-      [HttpGet("{id}/lamps/{lampId}")]
-      public Lamp GetLamp(int id, int lampId)
-      {
-         return trafficLightRepository.GetTrafficLightById(id).Lamps.SingleOrDefault(l => l.ID == lampId);
-      }
+        // GET api/<TrafficLightController>/5/lamps/2
+        [HttpGet("{id}/lamps/{lampId}")]
+        public Lamp GetLamp(int id, int lampId)
+        {
+            return trafficLightRepository.GetTrafficLightById(id).Lamps.SingleOrDefault(l => l.ID == lampId);
+        }
 
-      // PUT api/<TrafficLightController>/5
-      [HttpPut("{id}/lamps/{lampId}/{isOn}")]
-      public async Task Put(int id, int lampId, bool isOn)
-      {
-         var lamp = GetLamp(id, lampId);
+        // PUT api/<TrafficLightController>/5
+        [HttpPut("{id}/lamps/{lampId}/{isOn}")]
+        public async Task Put(int id, int lampId, bool isOn)
+        {
+            var lamp = GetLamp(id, lampId);
 
-         if (lamp != null)
-         {
+            if (lamp != null)
+            {
+                if (lamp.IsOn != isOn)
+                {
+                    await SetLampStateAsync(id, lampId, isOn, lamp);
+                }
+                else
+                {
+                   logger.LogInformation($"Lamp {lampId} is already in state {isOn} - skipping...");
+                }
+            }
+            else
+            {
+                logger.LogInformation($"Failed to put lamp {lampId} into state {isOn} - Lamp could not be found");
+            }
+        }
+
+        private async Task SetLampStateAsync(int id, int lampId, bool isOn, Lamp lamp)
+        {
             lamp.SetLampState(isOn);
             var updateClients = trafficLightHub.Clients.All.SendAsync("trafficLightUpdate", id);
 
             clewareLampControl.SwitchLight(id, lampId, isOn);
 
             await updateClients;
-         }
-      }
-   }
+        }
+    }
 }
